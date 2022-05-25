3e6991ca1aa99755c3b224cf112d515cf7e88d81
alaminyusuf <alaminyusuf131@gmail.com>
2022-05-25 20:41:08 +0100

    Cleanup

--- server.ts
+++ server.ts
@@ -1,45 +1,45 @@
-import express, { Request, Response } from "express";
-import nodemailer from "nodemailer";
-import multer from "multer";
-import { config } from "dotenv";
-import { ITicket } from "./types";
+import express, { Request, Response } from 'express';
+import nodemailer from 'nodemailer';
+import multer from 'multer';
+import { config } from 'dotenv';
+import { ITicket } from './types';
 
 config();
 const server = express();
 const upload = multer();
 const mailer = nodemailer.createTransport({
-	host: "smtp.gmail.com",
-	port: 465,
-	secure: true,
-	auth: {
-		user: process.env.EMAIL,
-		pass: process.env.PASS,
-	},
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
 });
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
-server.use("/public", express.static(process.cwd() + "/public"));
+server.use('/public', express.static(process.cwd() + '/public'));
 
-server.get("/", (req: Request, res: Response) => {
-	res.sendFile(process.cwd() + "/public/index.html");
+server.get('/', (req: Request, res: Response) => {
+  res.sendFile(process.cwd() + '/public/index.html');
 });
 
-server.post("/send", upload.none(), (req: Request, res: Response) => {
-	const data: ITicket = req.body;
-	const mail = {
-		sender: `${data.name} <${data.email}>`,
-		to: process.env.EMAIL,
-		subject: data.subject,
-		html: `<h1>${data.name}</h1> <p> ${data.email} </p> \n<p style="font-size: 1.4rem">${data.message}</p>`,
-	};
-	mailer.sendMail(mail, (err, data) => {
-		if (err) {
-			res.status(500).send("Something went wrong. ❕");
-		} else {
-			res.status(201).redirect("/");
-		}
-	});
+server.post('/send', upload.none(), (req: Request, res: Response) => {
+  const data: ITicket = req.body;
+  const mail = {
+    sender: data.name,
+    to: process.env.EMAIL,
+    subject: data.subject,
+    html: `<h1>${data.name}</h1> <p> ${data.email} </p> \n<p style="font-size: 1.4rem">${data.message}</p>`,
+  };
+  mailer.sendMail(mail, (err, _) => {
+    if (err) {
+      res.status(500).send('Something went wrong. ❕');
+    } else {
+      res.status(201).redirect('/');
+    }
+  });
 });
 
 module.exports = server;
--- test/server.test.ts
+++ test/server.test.ts
@@ -1,22 +1,19 @@
-process.env.NODE_ENV = "test";
-const nodemailer = require("nodemailer");
-const express = require("express");
-const request = require("supertest");
-const server = require("../server");
+process.env.NODE_ENV = 'test';
+const nodemailer = require('nodemailer');
+const express = require('express');
+const request = require('supertest');
+const server = require('../server');
 
-describe("Ticket module", () => {
-	const data = {
-		email: "test@test.com",
-		message: "this is from jest",
-		name: "jest",
-		subject: "jest test",
-	};
-	it("should send an email", async () => {
-		server.use(express.json());
-		const res = await request(server)
-			.post("/send")
-			.send({ ...data });
-		console.log(res.body);
-		expect(res.statusCode).toEqual(302);
-	}, 30000);
+describe('Ticket module', () => {
+  const data = {
+    email: 'test@test.com',
+    message: 'this is from jest',
+    name: 'jest',
+    subject: 'jest test',
+  };
+  it('should send an email', async () => {
+    server.use(express.json());
+    const res = await request(server).post('/send').send(data);
+    expect(res.statusCode).toEqual(302);
+  }, 30000);
 });
